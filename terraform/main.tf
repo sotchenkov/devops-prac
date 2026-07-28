df5150eb4d80f8a49fa3b99261f386f45bc3d95f
sotchenkov <sotchenkoff@gmail.com>
2026-07-28 22:39:18 +0300

    add: init terraform with docker-provider

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 1.15.0"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 4.5.0"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
