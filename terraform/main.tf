e518b3e7aec9f88a7b96e092ea3dca8d4ee157c0
sotchenkov <sotchenkoff@gmail.com>
2026-07-29 20:34:50 +0300

    add: create docker backend pool with for_each

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,16 +12,41 @@ provider "docker" {
   context = "colima"
 }
 
-resource "docker_image" "nginx" {
-  name = "nginx:1.31"
+resource "docker_image" "traefik" {
+  name = "traefik/whoami:v1.11.0"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "homelab"
+resource "docker_network" "backend" {
+  name = "homelab-backend"
+}
+
+resource "docker_container" "backend" {
+  for_each = {
+    "backend-01" = {
+      "name" : "homelab-backend-01"
+      "external_port" : 8080
+    }
+    "backend-02" = {
+      "name" : "homelab-backend-02"
+      "external_port" : 8081
+    }
+    "backend-03" = {
+      "name" : "homelab-backend-03"
+      "external_port" : 8082
+    }
+  }
+
+  image = docker_image.traefik.image_id
+  name  = each.value.name
+
+  networks_advanced {
+    name = docker_network.backend.name
+  }
+
   ports {
     internal = 80
-    external = 8080
+    external = each.value.external_port
   }
+
   restart = "unless-stopped"
 }
